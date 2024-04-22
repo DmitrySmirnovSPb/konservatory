4edeea4188e32288278db778d576c5361f3c54eb
DmitrySmirnovSPb <jobdsmirnov@yandex.ru>
2024-04-22 12:22:28 +0300

    2024-04-22 WORK

--- importModul/row_class.py
+++ importModul/row_class.py
@@ -1,4 +1,4 @@
-import sys, re, json, os
+import sys, re, json, os, datetime
 sys.path.append('C:/project/konservatory/data/')
 sys.path.append('C:/project/konservatory/importModul/')
 from DB_class import DB
@@ -6,26 +6,26 @@ from DB_class import DB
 class Row (object):
 
     data = {
-        'number':0,                     # ID в таблице report номер отчета
-        'in_the_chart':True,            # В графике или вне графика
+        'number':0,                     ### ID в таблице report номер отчета
+        'in_the_chart':True,            ### В графике или вне графика
         'call_Customer':None,           # Номер в заявке вызова заказчика
-        'room':'[]',                    # Номер помещания
-        'number_the_Customer':None,     # Номер позиции в заявке на вызов заказчика
+        'room':'[]',                    ### Номер помещания
+        'number_the_Customer':None,     ### Номер позиции в заявке на вызов заказчика
         'number_in_b_estimate':None,    # Предпологаемый номер по порядку в смете контракта в формате JSON
-        'number_in_order':None,         # Номер в отчёте
+        'number_in_order':None,         ### Номер в отчёте
         'name_id':None,                 # id Названия работы или материала
-        'dimension':None,               # id Единицы измерения
-        'value':None,                   # Количество фактически принятых работ
-        'code':None,                    # Шифр проекта
-        'date_of_the_call':None,        # Дата предъявления объёмов по графику
-        'actual_date':None,             # Фактическая дата приёмки
-        'id_contractor':None,           # id подрядчика по заявке
-        'id_actual_contractor':None,    # id исполнителя работ
-        'id_CC_engineer':None,          # id инженера строительного контроля принимавшиего работы
-        'result':False,                 # Результат предъявления работ:
-        'axes':None,                    # Оси в которых сдаётся объём в формате JSON
-        'floor':None,                   # Этаж и/или отметки
-        'note':None                     # Примечания к записи
+        'dimension':None,               ### id Единицы измерения
+        'value':None,                   ### Количество фактически принятых работ
+        'code':None,                    ### Шифр проекта
+        'date_of_the_call':None,        ### Дата предъявления объёмов по графику
+        'actual_date':None,             ### Фактическая дата приёмки
+        'id_contractor':None,           ### id подрядчика по заявке
+        'id_actual_contractor':None,    ### id исполнителя работ
+        'id_CC_engineer':None,          ### id инженера строительного контроля принимавшиего работы
+        'result':False,                 ### Результат предъявления работ:
+        'axes':None,                    ### Оси в которых сдаётся объём в формате JSON
+        'floor':None,                   ### Этаж и/или отметки
+        'note':None                     ### Примечания к записи
     }
 
     gap = '#^~'
@@ -50,8 +50,9 @@ class Row (object):
         self.getFloor()
         self.getDates()
         self.getCode()
-        self.getscopeOfWork()
-    
+        self.nameID()
+        self.getScopeOfWork()
+
     def getResult(self):
         if self.data['note'] == None:
             self.data['result'] = 1
@@ -94,11 +95,11 @@ class Row (object):
         if self.row['columnName'] == None:
             return result
         mat = r'[ ,(]\d{,2}[ ]?-?[ ]?\d{1,2}[ ]?[\\/и]{1}[ ]?[А-Я]{0,1}_?Н?[ ]?-?[ ]?[А-Я]{1}_?Н?|[ ,(]\d{,2}[ ]?-?[ ]?\d{1,2}[ ]?[\\/и]{1}[ ]?[А-Я]{0,1}_?Н?[ ]?-?[ ]?[А-Я]{1}_?Н?$|[ ,(][А-Я]{1}_?Н?[ ]?-?[ ]?[А-Я]{0,1}_?Н?[ ]?[\\/и]{1}[ ]?\d{,2}[ ]?-?[ ]?\d{1,2}|[ ,(][А-Я]{1}_?Н?[ ]?-?[ ]?[А-Я]{0,1}_?Н?[ ]?[\\/и]{1}[ ]?\d{,2}[ ]?-?[ ]?\d{1,2}$'
-        
+
         temp = re.findall(mat, self.row['columnName'])
         if len(temp) > 0:
             for f in temp: result.append(re.sub(r'[,(]','', str(f)))
-        
+
         self.data['axes'] = self.getAxes(result)
 
     def getAxes(self, splitList):
@@ -124,7 +125,7 @@ class Row (object):
                     slst[0], slst[1] = int(slst[0]), int(slst[1])
                 except:
                     pass
-                
+
                 if slst[0] > slst[1]:
                     slst[0], slst[1] = slst[1], slst[0]
             result.append(slst)
@@ -136,13 +137,13 @@ class Row (object):
             'id_actual_contractor':'executor',
             'id_CC_engineer':'colCCEngeneer'
         }
-        
+
         db = DB('polytechstroy')
 
         for key, value in list_Dict.items():
             string = self.row[value]
             if string == None: return None
-            
+
             if value == 'colCCEngeneer':
                 self.data[key] = self.getEngineerCC(string, db)
                 break
@@ -180,7 +181,7 @@ class Row (object):
         return id
 
     def replacementStr(self, string:str):
-        
+
         for old, new in self.replacement.items():
             string = string.replace(old,new)
 
@@ -198,39 +199,139 @@ class Row (object):
     def getFloor(self):
 
         text = self.row['columnName']
-        
+
         self.data['floor'] = {}
 
         mach_1 = r'([+-]\d+[\.,]\d{1,3})'
         altitude_mark = re.findall(mach_1, text)
         if len(altitude_mark) > 0:
             self.data['floor']['altitude_mark'] = altitude_mark
-        
-        mach_2 = r'(\d? ?(эт\.)|(\bэтаж) \d?)'
+        if 'подвал' in text:
+            self.data['floor']['floor'] = [-1]
+            return
+        mach_2 = r'\d?\s?эт\.?.*'
         temp = re.findall(mach_2, text)
         if type(temp) == list and len(temp) > 0:
             self.data['floor']['floor'] = list()
-            for fl in temp[0]:
+            for fl in temp:
+                # print(fl)
                 tempFloor = re.findall(r'\d', fl)
                 if len(tempFloor) > 0 :
-                    self.data['floor']['floor'].append(int(tempFloor[0][0]))
+                    self.data['floor']['floor'].append(int(tempFloor[0]))
                 else:
                     continue
         temp = json.loads(self.data['room'])
         if len(self.data['floor']) == 0 and len(temp) > 0:
             self.data['floor']['floor'] = int(temp[0].split('.')[0])
-    
+
     def getDates(self):
 
         self.data['date_of_the_call'] = self.row['plane']   # Дата предъявления объёмов по графику
         self.data['actual_date'] = self.row['fact'] if '---' not in str(self.row['fact']) else None         # Фактическая дата приёмки
 
-    def getCode(self):
+    def getIDCode(self, data):
+        temp = {}
 
-        self.data['code'] = self.row['workingDocumentationColumn']
+        date = re.findall (r'\d\d\.\d\d\.\d{4}',data['end']) # Проверка на наличие даты в шифре прокта
+        temp['date'] = []
+        if date != []:
+            for d in date:
+                temp['date'].append(str(datetime.datetime.strptime(d, '%d.%m.%Y')))
+                data['end'] = data['end'].replace(d, '').replace('от','').strip()
+        else:
+            temp['date'] = None
 
-    def getscopeOfWork(self):
+        sheet = re.findall (r'(\bл\.?.*\d+\.?d+\b)|(\bлист\.?.*\d+\.?d+\b)',data['end']) # Поверка на наличие укзаний на лист проекта code
+
+        if sheet != []:
+            if sheet[0][0] != '':
+                sheetRes = int(sheet[0][0].replace('л.',''))
+                data['end'] = data['end'].replace(sheet[0][0],'')
+            else:
+                sheetRes = int(sheet[0][1].replace('лист.*',''))
+                data['end'] = data['end'].replace(sheet[0][1])
+            temp['sheet'] = sheetRes
+        else:
+            temp['sheet'] = None
+
+        Keys = ['code','date']
+
+        tmp = []
+        if data['code'] != None:
+            tmp.append(data['code'])
+        if type(data['journal']) == list:
+            for d in data['journal']:
+                tmp.append(d)
+        elif data['journal'] != None:
+            tmp.append(data['journal'])
+        print(data)
+        print(tmp)
+        COP = [x for x in tmp if x != ''] # список шифров проекта или ЖАН
         
+        tempID = []
+        db = DB('polytechstroy')
+
+        for x in COP:
+            where = '`code` = "'+ x +'"'
+            id = db.select('code',{'where':[where],'columns':['id']})
+
+            if id == None:
+                Values =[x]
+                Values.append(temp['date'][0])
+                id = db.insert('code',[Keys,[Values]])#('dimension',[['name','multiplicity'],[[self.row['unitOfMeasurement'],mult]]])
+            
+            tempID.append(id)
+
+        temp['id'] = tempID
+        if len(data['end']) > 0:
+            temp['note'] = data['end']
+        else:
+            temp['note'] = None
+        self.data['code'] = json.dumps(temp)
+
+
+    def getCode(self):
+        result = {'code':None,'journal':None}
+        end = self.row['workingDocumentationColumn']
+        t = r'\b001[-/_]12-К-[А-Я]+[\. ]?\d*\.?\d*\b'
+        t1 = r'\bЖАН\s*№?\s*\d+\b'
+        pr = re.findall(t, end)
+        pr1 = re.findall(t1, end)
+        
+        if pr != []:
+            result['code'] = pr[0]
+            end = end.replace(result['code'], '')
+        if pr1 != [] and len(pr1) == 1:
+            result['journal'] = pr1[0]
+            end = end.replace(result['journal'], '')
+        elif len(pr1) > 1:
+            result['journal'] = []
+            for x in pr1:
+                end = end.replace(x, '')
+                result['journal'].append(x)
+        if len(end) > 0 and end[0] == ',':
+            end = end[1:]
+        
+        end = end.strip()
+
+        result['end'] = end
+        
+        self.getIDCode(result)
+
+    def getScopeOfWork(self):
+
         self.data['value'] = self.row['countColumn']        # Количество фактически принятых работ
 
-        self.data['dimension'] = self.row['unitOfMeasurement']
+        db = DB('polytechstroy')
+        id = db.select('dimension',{'where':['`name` = "' + str(self.row['unitOfMeasurement']) + '"'],'columns':['id']})
+        if id == None:
+            multiplicity = re.findall(r'\b\d+\b', self.row['unitOfMeasurement'])
+            if len(multiplicity) == 0:
+                mult = 1
+            else:
+                mult = int(multiplicity[0])
+            id = db.insert('dimension',[['name','multiplicity'],[[self.row['unitOfMeasurement'],mult]]])
+        self.data['dimension'] = id
+    
+    def nameID(self):
+        print(self.row['columnName'])
--- test.py
+++ test.py
@@ -4,34 +4,6 @@ from importModul.CC_Report_class import CC_Report
 from importModul.row_class import Row
 from importModul.report_class import Report
 
-def getRoom(text):
-    if text == None:
-        return False
-    mach = r'(\b\d\.\d\.\d{2}\w?)+'
-    lst = re.findall(mach, text)
-    return json.dumps(lst)
-
-def getFloor(text):
-    result = {}
-    mach_1 = r'([+-]\d+[\.,]\d{1,3})'
-    result['altitude_mark'] = re.findall(mach_1, text)
-    
-    mach_2 = r'(\d? ?(эт\.)|(\bэтаж) \d?)'
-    temp = re.findall(mach_2, text)
-    if type(temp) != list or len(temp) == 0:
-        return result
-    
-    result['floor'] = list()
-    for fl in temp[0]:
-        print(fl,'*******************************')
-        tempFloor = re.findall(r'\d', fl)
-        if len(tempFloor) > 0 :
-            result['floor'].append(int(tempFloor[0][0]))
-        else:
-            continue
-    print('*****')
-    return result
-
 if __name__ == '__main__':
 
     start_time = time.time()
@@ -69,7 +41,7 @@ if __name__ == '__main__':
     for item in result:
         scheduledCall = True            # Вызов по графику
         gc = CC_Report(link = item, globalLink = path, Sheet = 'Отчёт', nameDB = 'polytechstroy')
-        
+
         report = Report({})
 
         for key in report.data:
@@ -90,10 +62,10 @@ if __name__ == '__main__':
 
         flag = True
         for row in range(gc.startRow, gc.end + 1):
-            print('\n+----------------------------------------------------------------------+\n')
             if gc.Content[row][gc.columnNumber] == 'Вне графика' and flag:
                 flag = False
                 continue
+            print('\n+----------------------------------------------------------------------+\n')
             name = gc.Content[row][gc.columnName]
             tempDict = {'number':idReport,'in_the_chart':flag}
             for key in listick:
@@ -104,7 +76,7 @@ if __name__ == '__main__':
             tempDict['number_in_order'] = gc.Content[row][getattr(gc,'columnNumber')]
 
             rowClass = Row({row:tempDict})
-            if row > 100:
+            if row > 110:
                 exit(0)
         exit(0)
 
